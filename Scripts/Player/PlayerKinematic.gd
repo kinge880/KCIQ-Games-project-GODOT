365f96b210d0ed16be0a46d921c893265a0c8079
kinge880 <brunomaya10@hotmail.com>
2019-08-28 20:13:53 -0500

    att da escada
    att

--- Scripts/Player/PlayerKinematic.gd
+++ Scripts/Player/PlayerKinematic.gd
@@ -13,6 +13,8 @@ signal life_changed
 var delay_after_damage = false
 var walk_right
 var walk_left
+var walk_up
+var walk_down
 var jump
 var jump_stop
 var lower_move
@@ -26,6 +28,7 @@ var double_jump = false
 var jump_wall = false
 var in_ladder = false
 export var gravity = 500
+var gravity_aux = 0
 var velocity = Vector2()
 onready var anim = $Sprites
 
@@ -36,102 +39,136 @@ func _physics_process(delta):
 	if not jump_wall and not dash_delay: 
 		walk_right = Input.is_action_pressed("ui_right")
 		walk_left = Input.is_action_pressed("ui_left") 
+	walk_up = Input.is_action_pressed("ui_up")
+	walk_down = Input.is_action_pressed("ui_down") 
 	jump = Input.is_action_just_pressed("ui_up")
 	jump_stop = Input.is_action_just_released("ui_up")
-	lower_move = Input.is_action_just_pressed("ui_down") 
+	lower_move = Input.is_action_pressed("ui_down") 
 	dash = Input.is_action_just_pressed("ui_select")
 	atk = Input.is_action_just_pressed("ui_weak_attack")
 	
-	#agarra na parede, desliza e se solta
-	if on_wall and jump_wall_obted and lower_move and not is_on_floor():
-		velocity.y += gravity * delta
-		anim.play("fall")
-	elif on_wall and jump_wall_obted and not is_on_floor():
-		velocity.y = gravity * delta
-		anim.play("wall")
-	else:
-		#velocidade normal de queda, gravidade
-		velocity.y += gravity * delta
-	
-	if walk_right:
-		#salto na parede para a esquerda
-		if on_wall and anim.flip_h and not is_on_floor() and jump_wall_obted:
-			anim.play("jump")
-			velocity.x = speed * 1.5
-			velocity.y = -jump_force
-		#movimento basico direita
-		velocity.x = speed
-		anim.flip_h = false
-		if is_on_floor() and not dash_delay and not atk_delay:
-			anim.play("walk")
-		$ColissionWall.rotation_degrees = 270
-
-	elif walk_left:
-		#salto na parede para a direita
-		if on_wall and not anim.flip_h and not is_on_floor() and jump_wall_obted:
-			anim.play("jump")
-			velocity.x = -speed * 1.5
-			velocity.y = -jump_force
-		#movimento basico esquerda
-		velocity.x = -speed
-		anim.flip_h = true
-		if is_on_floor() and not dash_delay and not atk_delay:
-			anim.play("walk")
-		$ColissionWall.rotation_degrees = 90
+	#quando tocar em uma escada
+	if in_ladder:
 		
-	else:
-		velocity.x = 0
-		if is_on_floor() and not dash_delay and not atk_delay:
-			anim.play("idle_no_weapon")
-		
-	#pulo
-	if is_on_floor():
-		$DoubleJumpParticle.emitting = false
-		$DoubleJumpParticle2.emitting = false
-		air_dash = true
-		double_jump = true
-		if jump:
-			anim.play("jump")
-			velocity.y = -jump_force
-	elif jump_stop:
-		if velocity.y < 0:
-			velocity.y *= 0.5
-	
-	#pulo duplo
-	if not on_wall and double_jump and jump and double_jump_obted and not is_on_floor():
-		velocity.y = -jump_force
-		double_jump = false
-		anim.play("jump_roll")
-		$DoubleJumpParticle.emitting = true
-		$DoubleJumpParticle2.emitting = true
+		if walk_right:
+			#movimento basico direita
+			velocity.x = speed / 2
+			anim.flip_h = false
+			anim.play("ladder")
+			$ColissionWall.rotation_degrees = 270
 
-	#dash
-	if dash and air_dash and not dash_delay and dash_obted:
-		air_dash = false
-		anim.play("dash")
-		dash_delay = true
-		speed *= dash_multiplier
-		$DashTime.start()
+		elif walk_left:
+			#movimento basico esquerda
+			velocity.x = -speed / 2
+			anim.flip_h = true
+			anim.play("ladder")
+			$ColissionWall.rotation_degrees = 90
+		
+		elif walk_up:
+			#movimento basico esquerda
+			velocity.y = -speed / 3
+			anim.play("ladder")
+			$ColissionWall.rotation_degrees = 90
+		
+		elif walk_down:
+			#movimento basico esquerda
+			velocity.y = speed / 3
+			anim.play("ladder")
+			$ColissionWall.rotation_degrees = 90
+			
+		else:
+			velocity.x = 0
+			velocity.y = 0
+			anim.stop()
+	#quando sair da escada
+	else:
+		#agarra na parede, desliza e se solta
+		if on_wall and jump_wall_obted and lower_move and not is_on_floor():
+			velocity.y += gravity * delta
+			anim.play("fall")
+		elif on_wall and jump_wall_obted and not is_on_floor():
+			velocity.y = gravity * delta
+			anim.play("wall")
+		else:
+			#velocidade normal de queda, gravidade
+			velocity.y += gravity * delta
+		
+		if walk_right:
+			#salto na parede para a esquerda
+			if on_wall and anim.flip_h and not is_on_floor() and jump_wall_obted:
+				anim.play("jump")
+				velocity.x = speed * 1.5
+				velocity.y = -jump_force
+			#movimento basico direita
+			velocity.x = speed
+			anim.flip_h = false
+			if is_on_floor() and not dash_delay and not atk_delay:
+				anim.play("walk")
+			$ColissionWall.rotation_degrees = 270
+	
+		elif walk_left:
+			#salto na parede para a direita
+			if on_wall and not anim.flip_h and not is_on_floor() and jump_wall_obted:
+				anim.play("jump")
+				velocity.x = -speed * 1.5
+				velocity.y = -jump_force
+			#movimento basico esquerda
+			velocity.x = -speed
+			anim.flip_h = true
+			if is_on_floor() and not dash_delay and not atk_delay:
+				anim.play("walk")
+			$ColissionWall.rotation_degrees = 90
+			
+		else:
+			velocity.x = 0
+			if is_on_floor() and not dash_delay and not atk_delay:
+				anim.play("idle_no_weapon")
+			
+		#pulo
+		if is_on_floor():
+			$DoubleJumpParticle.emitting = false
+			$DoubleJumpParticle2.emitting = false
+			air_dash = true
+			double_jump = true
+			if jump:
+				anim.play("jump")
+				velocity.y = -jump_force
+		elif jump_stop:
+			if velocity.y < 0:
+				velocity.y *= 0.5
+		
+		#pulo duplo
+		if not on_wall and double_jump and jump and double_jump_obted and not is_on_floor():
+			velocity.y = -jump_force
+			double_jump = false
+			anim.play("jump_roll")
+			$DoubleJumpParticle.emitting = true
+			$DoubleJumpParticle2.emitting = true
+	
+		#dash
+		if dash and air_dash and not dash_delay and dash_obted:
+			air_dash = false
+			anim.play("dash")
+			dash_delay = true
+			speed *= dash_multiplier
+			$DashTime.start()
+		
+		#animação de queda
+		if velocity.y > 0 and not is_on_floor() and anim.get_animation() != "wall" and not dash_delay and not atk_delay:
+			anim.play("fall")
+			
+		#ataque
+		if atk and not is_on_floor():
+			$AtkTime.start()
+			atk_delay = true
+			anim.play("air_atack2")
+		elif atk and not atk_delay:
+			$AtkTime.start()
+			atk_delay = true
+			anim.play("atack1")
 	
 	velocity = move_and_slide(velocity, Vector2(0,-1))
 	
-	#animação de queda
-	if velocity.y > 0 and not is_on_floor() and anim.get_animation() != "wall" and not dash_delay and not atk_delay:
-		anim.play("fall")
-		
-	#ataque
-	if atk and not is_on_floor():
-		$AtkTime.start()
-		atk_delay = true
-		anim.play("air_atack2")
-	elif atk and not atk_delay:
-		$AtkTime.start()
-		atk_delay = true
-		anim.play("atack1")
-		
-	if in_ladder:
-		anim.play("ladder")
-		
 #emite o sinal que altera o valor da vida no HUD
 func life_changed():
 	emit_signal('life_changed', current_life)
@@ -172,9 +209,10 @@ func _on_ladderArea_body_entered(body):
 	velocity.x = 0
 	velocity.y = 0
 	in_ladder = true
+	gravity_aux = gravity
 	gravity = 0
-
 
 func _on_ladderArea_body_exited(body):
 	in_ladder = false
-	gravity = 500
+	gravity = gravity_aux
+	gravity_aux = 0
